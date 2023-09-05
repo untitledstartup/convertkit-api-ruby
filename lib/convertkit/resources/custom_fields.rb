b78a4d2b741afefb1ed845d3df8fb544fa466209
joelmergerssa <joel@mightynetworks.com>
2023-09-05 13:53:11 -0700

    Add support retrieving a list of custom fields on an account.

--- lib/convertkit/resources/custom_fields.rb
+++ lib/convertkit/resources/custom_fields.rb
@@ -6,6 +6,16 @@ module ConvertKit
       def initialize(client)
         @client = client
       end
+
+      # Returns a list of custom fields for the account.
+      # See https://developers.convertkit.com/#list-fields for details
+      def list
+        response = @client.get(PATH)
+
+        response['custom_fields'].map do |custom_field|
+          CustomFieldResponse.new(custom_field)
+        end
+      end
     end
   end
 end
--- spec/lib/convertkit/resources/custom_fields_spec.rb
+++ spec/lib/convertkit/resources/custom_fields_spec.rb
@@ -1,3 +1,27 @@
-describe ConvertKit::Resources::CustomField do
+describe ConvertKit::Resources::CustomFields do
+  include Validators::CustomFieldValidators
 
+  let(:client) { double('client') }
+
+  describe '#initialize' do
+    it 'sets the client' do
+      custom_fields = ConvertKit::Resources::CustomFields.new(client)
+      expect(custom_fields.instance_variable_get(:@client)).to be(client)
+    end
+  end
+
+  describe '#list' do
+    let(:custom_fields) { ConvertKit::Resources::CustomFields.new(client) }
+
+    it 'returns a list of custom fields' do
+      response = { 'custom_fields' => [
+        {'id' => 1, 'name' => 'ck_field_name1', 'key' => 'test_key1', 'label' => 'test_label_value1' },
+        {'id' => 2, 'name' => 'ck_field_name2', 'key' => 'test_key2', 'label' => 'test_label_value2' },
+      ]}
+
+      expect(client).to receive(:get).with('custom_fields').and_return(response)
+      custom_fields_response = custom_fields.list
+      validate_custom_fields(custom_fields_response, response['custom_fields'])
+    end
+  end
 end
