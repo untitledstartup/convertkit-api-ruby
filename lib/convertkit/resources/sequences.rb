b025a05d6c7f43cfe6cf1e3a98f5cf32aaee566c
joelmergerssa <joel@mightynetworks.com>
2023-09-05 17:55:55 -0700

    Add support to retrieve a add a subscriber to a sequence.

--- lib/convertkit/resources/sequences.rb
+++ lib/convertkit/resources/sequences.rb
@@ -17,6 +17,27 @@ module ConvertKit
           ConvertKit::Resources::SequenceResponse.new(sequence)
         end
       end
+
+      # Subscribes an email address to a sequence.
+      # See https://developers.convertkit.com/#add-subscriber-to-a-sequence for details
+      # @param [Integer] id Sequence ID
+      # @param [String] email Subscriber's email address
+      # @param [Hash] options
+      # @option options [String] :first_name Subscriber's first name
+      # @option options [Hash] :fields  Key value pairs for existing custom fields
+      # @option options [Array<Integer>] :tags Array of tag ids to subscribe to
+      def add_subscriber(id, email, options = {})
+        request = {
+          email: email,
+          first_name: options[:first_name],
+          fields: options[:fields],
+          tags: options[:tags]
+        }.compact
+
+        response = @client.post("#{PATH}/#{id}/subscribe", request)
+
+        ConvertKit::Resources::SubscriptionResponse.new(response)
+      end
     end
   end
 end
--- spec/lib/convertkit/resources/sequences_spec.rb
+++ spec/lib/convertkit/resources/sequences_spec.rb
@@ -1,5 +1,6 @@
 describe ConvertKit::Resources::Sequences do
   include Validators::SequenceValidators
+  include Validators::SubscriptionValidators
 
   let(:client) { double('client') }
 
@@ -22,6 +23,29 @@ describe ConvertKit::Resources::Sequences do
       expect(client).to receive(:get).with('sequences').and_return(response)
       sequences_response = sequences.list
       validate_sequences(sequences_response, response['courses'])
+    end
+  end
+
+  describe '#add_subscriber' do
+    let(:sequences) { ConvertKit::Resources::Sequences.new(client) }
+
+    it 'adds a subscriber to a sequence' do
+      response = {
+        'id' => 1,
+        'state' => 'active',
+        'subscribable_id' => 2,
+        'subscribable_type' => 'course',
+        'subscriber' => { 'id' => 3 },
+        'created_at' => '2023-08-09T04:30:00Z'
+      }
+      options = {first_name: 'name', fields: { field_key: 'field_value' }, tags: [1,2]}
+      expect(client).to receive(:post).with(
+        'sequences/1/subscribe',
+        { email: 'test_email', first_name: 'name', fields: { field_key: 'field_value' }, tags:[1,2]}
+      ).and_return(response)
+
+      subscription_response = sequences.add_subscriber(1, 'test_email', options)
+      validate_subscription(subscription_response, response)
     end
   end
 end
