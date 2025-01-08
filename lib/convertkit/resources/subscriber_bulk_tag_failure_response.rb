0f6d498ba85b2239d9bcd15d4a247209db044a28
Guilherme Martini <guilherme.martini20@gmail.com>
2025-01-08 14:20:11 -0300

    add bulk remove tag from subscriber method

---
+++ lib/convertkit/resources/subscriber_bulk_tag_failure_response.rb
@@ -0,0 +1,12 @@
+module ConvertKit
+  module Resources
+    class SubscriberBulkTagFailureResponse
+      attr_accessor :subscriber, :errors
+
+      def initialize(response)
+        @subscriber = SubscriberResponse.new(response['subscriber']) if response['subscriber']
+        @errors = response['errors']
+      end
+    end
+  end
+end
